6c42337187c4303ae0050ed00348b5480e643997
Buddhika Dhananjaya <dhananjaya_gw@yahoo.com>
2020-03-25 10:41:24 +0530

    Added Report.cs ReportSchedule.cs FileContext.cs

---
+++ Project/FileScheduleProject/FileScheduleProject/Models/Report.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FileScheduleProject.Models
+{
+    public class Report
+    {
+        [Key]
+        public int ReportID { get; set; }
+        [Required]
+        public string ReportName { get; set; }
+        public string ReportType { get; set; }
+    }
+}
