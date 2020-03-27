e2a87f2c2fcbee2a807ca89d5bfae478c798d6f3
Tharusara <tharusaranugegoda@gmail.com>
2020-03-28 00:59:34 +0530

    fix

--- Project/FileSchedule/file-schedule-app/src/app/files/filelist/filelist.component.ts
+++ Project/FileSchedule/file-schedule-app/src/app/files/filelist/filelist.component.ts
@@ -1,13 +1,8 @@
-<<<<<<< HEAD
-import { Component, OnInit } from '@angular/core';
-import { files } from 'src/app/Models/file.model';
-=======
 import { Component, OnInit, ViewChild } from '@angular/core';
 import {MatPaginator} from '@angular/material/paginator';
 import {MatSort} from '@angular/material/sort';
 import {MatTableDataSource} from '@angular/material/table';
 import { FileData } from 'src/app/models/FileData';
->>>>>>> 990d5e926a75df6541b224fc2df5eb08e51637fe
 
 @Component({
   selector: 'app-filelist',
@@ -15,38 +10,7 @@ import { FileData } from 'src/app/models/FileData';
   styleUrls: ['./filelist.component.css']
 })
 export class FilelistComponent implements OnInit {
-<<<<<<< HEAD
-files: files[] = [];
-Name: string;
-dataSource: File;
-  constructor() { }
 
-  ngOnInit(): void {
-    this.files = [{
-      fileID: 1,
-      fileName: 'FileONe',
-      filePath: 'c:/files',
-      catagory: 'Financial',
-  },
-  {
-    fileID: 2,
-    fileName: 'TwoFile',
-    filePath: 'c:/files/filelist',
-    catagory: 'Accounts',
-}];
-  }
-  Search() {
-    this.files = this.files.filter(res => {
-      return res.fileName.toLocaleLowerCase().match(this.Name.toLocaleLowerCase());
-    });
-  }
-
-  OpenFolder() {
-
-  }
-  onEdit() {
-
-=======
   displayedColumns: string[] = ['FileName', 'FilePath', 'Category'];
   files: FileData[] = [];
   dataSource: MatTableDataSource<FileData>;
@@ -55,7 +19,7 @@ dataSource: File;
   @ViewChild(MatSort, {static: true}) sort: MatSort;
 
   constructor() {
-    //Call the get all method here to get the data and bind it.
+    // Call the get all method here to get the data and bind it.
     this.dataSource = new MatTableDataSource(this.files);
    }
 
@@ -71,9 +35,6 @@ dataSource: File;
     if (this.dataSource.paginator) {
       this.dataSource.paginator.firstPage();
     }
->>>>>>> 990d5e926a75df6541b224fc2df5eb08e51637fe
   }
-  onDelete() {
 
-  }
 }
