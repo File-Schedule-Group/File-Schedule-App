56a42b09c73d17aa065b98cd5ee29b023165ca6c
Buddhika Dhananjaya <dhananjaya_gw@yahoo.com>
2020-04-20 18:42:39 +0530

    Added reportlist module

--- Project/FileSchedule/file-schedule-app/src/app/app-routing.module.ts
+++ Project/FileSchedule/file-schedule-app/src/app/app-routing.module.ts
@@ -2,7 +2,8 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [{ path: 'dashboard', loadChildren: () => import('./dashboard/dashboard.module').then(m => m.DashboardModule) }, 
-                        { path: 'files', loadChildren: () => import('./files/files.module').then(m => m.FilesModule) }];
+                        { path: 'files', loadChildren: () => import('./files/files.module').then(m => m.FilesModule) },
+                        { path: 'reports', loadChildren: () => import('./reports/reports.module').then(m => m.ReportsModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
